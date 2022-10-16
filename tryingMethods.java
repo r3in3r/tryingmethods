70f5e2f8edcd34f3e2e4ce618bd5fb7cacbf8dda
Rainer Mbongo <rainermbongo@gmail.com>
2022-10-16 13:05:47 +0000

    added switch case

---
+++ tryingMethods.java
@@ -0,0 +1,40 @@
+//this is a class that put methods to try
+public class tryingMethods {
+    static void daysOfTheWeek()
+    {
+        int days=5;
+        switch (days) {
+                case 1:
+            System.out.println("Monday");    
+                break;
+                case 2:
+            System.out.println("Tuesday");    
+                break;
+                case 3:
+            System.out.println("Wednesday");    
+                break;
+                case 4:
+            System.out.println("Thursday");    
+                break;
+                case 5:
+            System.out.println("Friday");    
+                break;
+                case 6:
+            System.out.println("Saturday");    
+                break;
+                case 7:
+            System.out.println("Sunday");    
+                break;
+            
+
+            
+        }
+
+        System.out.println("");
+    }
+    public static void main(String[] args) {
+        daysOfTheWeek();
+
+        
+    }
+}
